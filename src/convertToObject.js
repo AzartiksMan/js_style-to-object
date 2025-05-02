a990eb92a5ced388b1951c9fc088cbc4ef9122ac
Denis Kasatskiy <azartofgod@gmail.com>
2025-05-02 15:09:20 +0300

    Add Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const rawCssStylesArray = sourceString
+    .split(';')
+    .map((str) => str.trim())
+    .filter((item) => item.length > 0);
+
+  const cssStylesPairsKeyPlusValue = rawCssStylesArray
+    .map((str) => str.split(':').filter((item) => item !== ''))
+    .map(([key, value]) => [key.trim(), value.trim()]);
+
+  const cssStylesObject = cssStylesPairsKeyPlusValue.reduce(
+    (acc, [key, value]) => {
+      acc[key] = value;
+
+      return acc;
+    },
+    {},
+  );
+
+  return cssStylesObject;
 }
 
 module.exports = convertToObject;
